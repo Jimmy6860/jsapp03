c0f9db91109652b6c7a598d6a5a49db66d84d155
Jimmy Lam <jimmy6860@gmail.com>
2020-03-13 23:26:56 +0100

    Tasks are visible now

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-document.getElementById('todoInputForm').addEventListener('submit', addTodo());
+document.getElementById('todoInputForm').addEventListener('submit', addTodo);
 
 function addTodo(e) {
     let todoId = chance.guid();
     let todoTitle = document.getElementById('todo-title').value;
     let todoDec = document.getElementById('todo-description').value;
-    let todoPrio = document.getElementsByClassName('todo-priority').value;
+    let todoPrio = document.getElementById('todo-priority').value;
     let todoStatus = 'Open';
 
     let todo = {
         id: todoId,
         title: todoTitle,
-        description: todoDec,
+        desc: todoDec,
         priority: todoPrio,
         status: todoStatus 
     }
@@ -25,10 +25,39 @@ function addTodo(e) {
         localStorage.setItem('todos', JSON.stringify(todos))
     }
 
-    console.log(JSON.parse(localStorage.getItem('todos')))
+    document.getElementById('todoInputForm').reset()
+    fetchTodosData()
+    console.log(localStorage.getItem('todos'))
+    e.preventDefault();
 }
 
 function fetchTodosData() {
     let todos = JSON.parse(localStorage.getItem('todos'))
+    let todosList = document.getElementById('todos-overview');
+    
+    console.log(todos, todos.length)
+    todosList.innerHTML = '';
+
+    for (let i = todos.length-1 ; i >= 0; i--) {
+        let id = todos[i].id;
+        let title = todos[i].title;
+        let desc = todos[i].desc;
+        let priority = todos[i].priority;
+        let status = todos[i].status;
+
+        let setPriority = () => {
+            if(priority === 'Low') {
+                return 'todo-item-low'
+            } else if (priority === 'Middle') {
+                return 'todo-item-middle'
+            } else if (priority === 'High') {
+                return 'todo-item-high'
+            }
+        }
+        
+        todosList.innerHTML += `<div class="col-11 card bg-white mt-4 justify-content-center ${setPriority()}" style="height: 50px;">
+                                    ${title}
+                                </div> `
+    }
 
 }
